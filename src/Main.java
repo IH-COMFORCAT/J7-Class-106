9ee2030d21ad08162507d6db80dfd8c6209021fc
Jaume Sánchez <jasato89@gmail.com>
2022-09-21 10:06:05 +0200

    Standard input

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,39 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Por favor, introduce tu nombre:");
+        String name = input.nextLine();
+
+        System.out.println(name);
+
+        System.out.print("Por favor, introduce tu edad:");
+        int age = input.nextInt();
+
+        while (age < 0) {
+            System.err.println("Tu edad no puede ser negativa");
+            age = input.nextInt();
+        }
+
+        if (age >= 18) System.out.println("Eres mayor de edad");
+        if (age < 18) System.out.println("Eres un yogurin");
+
+
+        System.out.print("Tienes " + age + " años");
+
+
+        String frase = input.nextLine();
+        //El perro de San Roque no tiene rabo o quizas si
+        //perro no empieza por vocal
+        //de no empieza por vocal
+        //San no empieza por vocal
+        //..
+        //Total palabras palabras que empiezan por vocal: 2
+
+
+
     }
 }
